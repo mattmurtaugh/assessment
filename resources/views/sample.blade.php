0cf7c67ec84e36279d9341f948267b79cb5a3518
Tommy McGuire <tommy@modernmcguire.com>
2022-01-19 15:30:43 -0600

    + Sample app UI shell

---
+++ resources/views/sample.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+	<div class="container my-5">
+		<div class="row">
+			<div class="col-md-6">
+				<div class="card">
+					<div class="card-body">
+						<h1>Welcome! <i class="fa fa-rocket"></i></h1>
+						<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita, nisi. Nam sunt sapiente fuga repellendus, facere, autem iure, consequuntur ea hic cumque distinctio perferendis nesciunt, aperiam recusandae quas expedita! Corporis!</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+@endsection
