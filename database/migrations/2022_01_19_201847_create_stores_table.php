6f08a38dee92e148cece7880320cfc542ca539f8
Tommy McGuire <tommy@modernmcguire.com>
2022-01-19 15:29:16 -0600

    + Starter models and migrations

---
+++ database/migrations/2022_01_19_201847_create_stores_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStoresTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stores', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('brand_id');
+            $table->unsignedBigInteger('number');
+            $table->string('address');
+            $table->string('city');
+            $table->string('state');
+            $table->string('zip_code');
+
+            $table->timestamps();
+
+            $table->foreign('brand_id')->references('id')->on('brands');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stores');
+    }
+}
